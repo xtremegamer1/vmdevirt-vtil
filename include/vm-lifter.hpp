7c60eb3439823b2caa357e21fe77c3eb5de32472
xtremegamer1 <rlyman308@gmail.com>
2022-10-21 03:28:43 -0600

    Working on adding lifer from vminstrs to VTIL.

--- include/vm-lifter.hpp
+++ include/vm-lifter.hpp
@@ -4,17 +4,19 @@
 
 namespace vm
 {
-class lifter
+class lifter_t
 {
   public:
-  lifter(const vm::instrs::vrtn_t* const routine);
+  lifter_t(const vm::instrs::vrtn_t* const routine, const vm::vmctx_t* const ctx);
   bool lift();
   const vtil::routine& get_routine();  
   private:
+  const std::uintptr_t load_delta;
+  const std::array<ZydisRegister, 16> vmentry_pushes;
   const vm::instrs::vrtn_t* const vmp_routine;
   vtil::routine* vtil_routine;
   vtil::basic_block* current_block;
-  uint64_t reg_id;
+  uint64_t reg_id = 0;
   vtil::register_desc* scratch_regs[32];
   void lift_handler(vm::instrs::vinstr_t v_instr);
 };
--- src/vm-lifter.cpp
+++ src/vm-lifter.cpp
@@ -1,29 +1,60 @@
-/* #include <vm-lifter.hpp>
+#include <vm-lifter.hpp>
 
-  static constexpr vtil::register_desc FLAG_CF = vtil::REG_FLAGS.select( 1, 0 );
-  static constexpr vtil::register_desc FLAG_PF = vtil::REG_FLAGS.select( 1, 2 );
-  static constexpr vtil::register_desc FLAG_AF = vtil::REG_FLAGS.select( 1, 4 );
-  static constexpr vtil::register_desc FLAG_ZF = vtil::REG_FLAGS.select( 1, 6 );
-  static constexpr vtil::register_desc FLAG_SF = vtil::REG_FLAGS.select( 1, 7 );
-  static constexpr vtil::register_desc FLAG_DF = vtil::REG_FLAGS.select( 1, 10 );
-  static constexpr vtil::register_desc FLAG_OF = vtil::REG_FLAGS.select( 1, 11 );
-
+static constexpr vtil::register_desc FLAG_CF = vtil::REG_FLAGS.select( 1, 0 );
+static constexpr vtil::register_desc FLAG_PF = vtil::REG_FLAGS.select( 1, 2 );
+static constexpr vtil::register_desc FLAG_AF = vtil::REG_FLAGS.select( 1, 4 );
+static constexpr vtil::register_desc FLAG_ZF = vtil::REG_FLAGS.select( 1, 6 );
+static constexpr vtil::register_desc FLAG_SF = vtil::REG_FLAGS.select( 1, 7 );
+static constexpr vtil::register_desc FLAG_DF = vtil::REG_FLAGS.select( 1, 10 );
+static constexpr vtil::register_desc FLAG_OF = vtil::REG_FLAGS.select( 1, 11 );
 namespace vm
 {
-  lifter::lifter(const vm::instrs::vrtn_t* const routine) : vmp_routine(routine), reg_id(0) {};
+  lifter_t::lifter_t(const vm::instrs::vrtn_t* const routine, const vm::vmctx_t* const ctx) : 
+    vmp_routine(routine), vmentry_pushes(ctx->get_vmentry_push_order()), load_delta(ctx->m_image_load_delta) {};
 
-  bool lifter::lift()
-  {
+  static inline void allocate_scratch(vtil::register_desc* scratch_regs[], uint64_t& reg_id){
     for (int i = 0; i < 32; ++i)
     {
       scratch_regs[i] = new vtil::register_desc(vtil::register_virtual, reg_id++, 64);
     }
-    current_block = vtil::basic_block::begin(0);
+  }
+  static inline void deallocate_scratch(vtil::register_desc* scratch_regs[]) {
+    for (int i = 0; i < 32; ++i)
+    {
+      delete scratch_regs[i];
+    }
+  }
+
+  bool lifter_t::lift()
+  {
+    allocate_scratch(scratch_regs, reg_id);
+    //The rva of first block is the first instruction after the vmenter
+    current_block = vtil::basic_block::begin(vmp_routine->m_blks[0].m_vip.rva);
+    //VMENTER pushes all gp registers and RFLAGS to the stack which is inherited by the virtual stack.
+    for (auto zyreg : vmentry_pushes)
+    {
+      current_block->push(zyreg);
+    }
+    //Load delta is pushed to the stack
+    current_block->push(load_delta);
     vtil_routine = current_block->owner;
+    //Lift the handlers basically
+    for (const auto& blk : vmp_routine->m_blks)
+    {
+      for (const auto& instr : blk.m_vinstrs)
+      {
+        lift_handler(instr);
+      }
+    }
+    for (auto r_it = vmentry_pushes.rbegin(); r_it != vmentry_pushes.rend(); ++r_it)
+    {
+      current_block->pop(*r_it);
+    }
+    deallocate_scratch(scratch_regs);
     return true;
   }
 
-  void lifter::lift_handler(vm::instrs::vinstr_t v_instr)
+  void lifter_t::lift_handler(vm::instrs::vinstr_t v_instr)
   {
     switch (v_instr.mnemonic)
     {
@@ -131,4 +162,4 @@ namespace vm
       }
     }
   }
-} */
+}
